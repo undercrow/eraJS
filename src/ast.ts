bbb26c9350f386e13f9ec6b878addeba8b3ba9da
Undercrow <undercrow@protonmail.com>
2020-10-22 11:44:32 +0900

    Implement zero argument commands in parser

--- src/ast.ts
+++ src/ast.ts
@@ -1,13 +1,13 @@
-export type BaseNode<T extends string> = {
+type BaseStatement<T extends string> = {
 	type: T;
 };
-function base<T extends string>(type: T): BaseNode<T> {
+function base<T extends string>(type: T): BaseStatement<T> {
 	return {
 		type,
 	};
 }
 
-type Label = BaseNode<"label"> & {
+export type Label = BaseStatement<"label"> & {
 	name: string;
 };
 export const label = (name: string): Label => ({
@@ -15,7 +15,7 @@ export const label = (name: string): Label => ({
 	name,
 });
 
-type Goto = BaseNode<"goto"> & {
+export type Goto = BaseStatement<"goto"> & {
 	dest: string;
 };
 export const goto = (dest: string): Goto => ({
@@ -23,4 +23,106 @@ export const goto = (dest: string): Goto => ({
 	dest,
 });
 
-export type Node = Label | Goto;
+type BaseCommand<T> = BaseStatement<"command"> & {
+	command: T;
+};
+const command = <T>(cmd: T): BaseCommand<T> => ({
+	...base("command"),
+	command: cmd,
+});
+export type DrawLine = BaseCommand<"drawline">;
+export const drawLine = (): DrawLine => command("drawline");
+export type ResetColor = BaseCommand<"resetcolor">;
+export const resetColor = (): ResetColor => command("resetcolor");
+export type ResetBgColor = BaseCommand<"resetbgcolor">;
+export const resetBgColor = (): ResetBgColor => command("resetbgcolor");
+export type GetColor = BaseCommand<"getcolor">;
+export const getColor = (): GetColor => command("getcolor");
+export type GetDefColor = BaseCommand<"getdefcolor">;
+export const getDefColor = (): GetDefColor => command("getdefcolor");
+export type GetBgColor = BaseCommand<"getbgcolor">;
+export const getBgColor = (): GetBgColor => command("getbgcolor");
+export type GetDefBgColor = BaseCommand<"getdefbgcolor">;
+export const getDefBgColor = (): GetDefBgColor => command("getdefbgcolor");
+export type GetFocusColor = BaseCommand<"getfocuscolor">;
+export const getFocusColor = (): GetFocusColor => command("getfocuscolor");
+export type GetStyle = BaseCommand<"getstyle">;
+export const getStyle = (): GetStyle => command("getstyle");
+export type GetFont = BaseCommand<"getfont">;
+export const getFont = (): GetFont => command("getfont");
+export type CurrentAlign = BaseCommand<"currentalign">;
+export const currentAlign = (): CurrentAlign => command("currentalign");
+export type CurrentRedraw = BaseCommand<"currentredraw">;
+export const currentRedraw = (): CurrentRedraw => command("currentredraw");
+export type PrintCPerLine = BaseCommand<"printcperline">;
+export const printCPerLine = (): PrintCPerLine => command("printcperline");
+export type LineIsEmpty = BaseCommand<"lineisempty">;
+export const lineIsEmpty = (): LineIsEmpty => command("lineisempty");
+export type IsSkip = BaseCommand<"isskip">;
+export const isSkip = (): IsSkip => command("isskip");
+export type MouseSkip = BaseCommand<"mouseskip">;
+export const mouseSkip = (): MouseSkip => command("mouseskip");
+export type AddDefChara = BaseCommand<"adddefchara">;
+export const addDefChara = (): AddDefChara => command("adddefchara");
+export type AddVoidChara = BaseCommand<"addvoidchara">;
+export const addVoidChara = (): AddVoidChara => command("addvoidchara");
+export type DelAllChara = BaseCommand<"delallchara">;
+export const delAllChara = (): DelAllChara => command("delallchara");
+export type ResetData = BaseCommand<"resetdata">;
+export const resetData = (): ResetData => command("resetdata");
+export type ResetGlobal = BaseCommand<"resetglobal">;
+export const resetGlobal = (): ResetGlobal => command("resetglobal");
+export type SaveGlobal = BaseCommand<"saveglobal">;
+export const saveGlobal = (): SaveGlobal => command("saveglobal");
+export type LoadGlobal = BaseCommand<"loadglobal">;
+export const loadGlobal = (): LoadGlobal => command("loadglobal");
+export type OutputLog = BaseCommand<"outputlog">;
+export const outputLog = (): OutputLog => command("outputlog");
+export type GetTime = BaseCommand<"gettime">;
+export const getTime = (): GetTime => command("gettime");
+export type GetMillisecond = BaseCommand<"getmillisecond">;
+export const getMillisecond = (): GetMillisecond => command("getmillisecond");
+export type GetSecond = BaseCommand<"getsecond">;
+export const getSecond = (): GetSecond => command("getsecond");
+export type ForceWait = BaseCommand<"forcewait">;
+export const forceWait = (): ForceWait => command("forcewait");
+export type WaitAnyKey = BaseCommand<"waitanykey">;
+export const waitAnyKey = (): WaitAnyKey => command("waitanykey");
+export type DumpRand = BaseCommand<"dumprand">;
+export const dumpRand = (): DumpRand => command("dumprand");
+export type InitRand = BaseCommand<"initrand">;
+export const initRand = (): InitRand => command("initrand");
+export type DebugClear = BaseCommand<"debugclear">;
+export const debugClear = (): DebugClear => command("debugclear");
+export type MouseX = BaseCommand<"mousex">;
+export const mouseX = (): MouseX => command("mousex");
+export type MouseY = BaseCommand<"mousey">;
+export const mouseY = (): MouseY => command("mousey");
+export type IsActive = BaseCommand<"isactive">;
+export const isActive = (): IsActive => command("isactive");
+export type CbgClear = BaseCommand<"cbgclear">;
+export const cbgClear = (): CbgClear => command("cbgclear");
+export type CbgClearButton = BaseCommand<"cbgclearbutton">;
+export const cbgClearButton = (): CbgClearButton => command("cbgclearbutton");
+export type CbgRemoveBmap = BaseCommand<"cbgremovebmap">;
+export const cbgRemoveBmap = (): CbgRemoveBmap => command("cbgremovebmap");
+export type ClearTextBox = BaseCommand<"cleartextbox">;
+export const clearTextBox = (): ClearTextBox => command("cleartextbox");
+export type StrData = BaseCommand<"strdata">;
+export const strData = (): StrData => command("strdata");
+export type StopCallTrain = BaseCommand<"stopcalltrain">;
+export const stopCallTrain = (): StopCallTrain => command("stopcalltrain");
+
+/* eslint-disable array-element-newline */
+export type PlainCommand =
+	| DrawLine
+	| ResetColor | ResetBgColor | GetColor | GetDefColor | GetBgColor | GetDefBgColor
+	| GetFocusColor | GetStyle | GetFont | CurrentAlign | CurrentRedraw | PrintCPerLine
+	| LineIsEmpty | IsSkip| MouseSkip | AddDefChara | AddVoidChara | DelAllChara | ResetData
+	| ResetGlobal | SaveGlobal | LoadGlobal | OutputLog | GetTime | GetMillisecond | GetSecond
+	| ForceWait | WaitAnyKey | DumpRand | InitRand | DebugClear | MouseX | MouseY | IsActive
+	| CbgClear | CbgClearButton | CbgRemoveBmap | ClearTextBox | StrData | StopCallTrain;
+/* eslint-enable array-element-newline */
+export type Command = PlainCommand;
+
+export type Statement = Label | Goto | Command;
--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-import * as P from "parsimmon";
+import P from "parsimmon";
 
 import * as ast from "./ast";
 
@@ -20,11 +20,14 @@ type LanguageSpec = {
 	Whitespace: null;
 	Whitespace0M: null;
 	Whitespace1M: null;
+	EOL: null;
 	Identifier: string;
-	Label: string;
-	Goto: string;
-	Line: ast.Node | null;
-	MultiLine: (ast.Node | null)[];
+	Label: ast.Label;
+	Goto: ast.Goto;
+	PlainCommand: ast.PlainCommand;
+	Command: ast.Command;
+	Statement: ast.Statement;
+	Language: ast.Statement[];
 };
 
 const language = P.createLanguage<LanguageSpec>({
@@ -32,17 +35,62 @@ const language = P.createLanguage<LanguageSpec>({
 	Whitespace: () => skip(P.alt(P.string(" "), P.string("\t"), P.string("　"))),
 	Whitespace0M: (r) => skip(r.Whitespace.many()),
 	Whitespace1M: (r) => skip(r.Whitespace.atLeast(1)),
+	EOL: (r) => P.alt(r.Whitespace, P.newline).atLeast(1).map(() => null),
 	Identifier: () => P.noneOf(SPECIAL_CHAR.join("")).atLeast(1).tie(),
-	Label: (r) => P.string("@").then(r.Identifier),
-	Goto: (r) => P.string("$").then(r.Identifier),
-	Line: (r) => P.alt(
-		r.Label.skip(r.Whitespace0M).map((val) => ast.label(val)),
-		r.Goto.skip(r.Whitespace0M).map((val) => ast.goto(val)),
+	Label: (r) => P.string("@").then(r.Identifier).map(ast.label).skip(r.EOL),
+	Goto: (r) => P.string("$").then(r.Identifier).map(ast.goto).skip(r.EOL),
+	PlainCommand: (r) => P.alt(
+		r.Whitespace0M.then(P.string("DRAWLINE")).map(ast.drawLine).skip(r.EOL),
+		r.Whitespace0M.then(P.string("RESETCOLOR")).map(ast.resetColor).skip(r.EOL),
+		r.Whitespace0M.then(P.string("RESETBGCOLOR")).map(ast.resetBgColor).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETCOLOR")).map(ast.getColor).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETDEFCOLOR")).map(ast.getDefColor).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETBGCOLOR")).map(ast.getBgColor).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETDEFBGCOLOR")).map(ast.getDefBgColor).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETFOCUSCOLOR")).map(ast.getFocusColor).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETSTYLE")).map(ast.getStyle).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETFONT")).map(ast.getFont).skip(r.EOL),
+		r.Whitespace0M.then(P.string("CURRENTALIGN")).map(ast.currentAlign).skip(r.EOL),
+		r.Whitespace0M.then(P.string("CURRENTREDRAW")).map(ast.currentRedraw).skip(r.EOL),
+		r.Whitespace0M.then(P.string("PRINTCPERLINE")).map(ast.printCPerLine).skip(r.EOL),
+		r.Whitespace0M.then(P.string("LINEISEMPTY")).map(ast.lineIsEmpty).skip(r.EOL),
+		r.Whitespace0M.then(P.string("ISSKIP")).map(ast.isSkip).skip(r.EOL),
+		r.Whitespace0M.then(P.string("MOUSESKIP")).map(ast.mouseSkip).skip(r.EOL),
+		r.Whitespace0M.then(P.string("ADDDEFCHARA")).map(ast.addDefChara).skip(r.EOL),
+		r.Whitespace0M.then(P.string("ADDVOIDCHARA")).map(ast.addVoidChara).skip(r.EOL),
+		r.Whitespace0M.then(P.string("DELALLCHARA")).map(ast.delAllChara).skip(r.EOL),
+		r.Whitespace0M.then(P.string("RESETDATA")).map(ast.resetData).skip(r.EOL),
+		r.Whitespace0M.then(P.string("RESETGLOBAL")).map(ast.resetGlobal).skip(r.EOL),
+		r.Whitespace0M.then(P.string("SAVEGLOBAL")).map(ast.saveGlobal).skip(r.EOL),
+		r.Whitespace0M.then(P.string("LOADGLOBAL")).map(ast.loadGlobal).skip(r.EOL),
+		r.Whitespace0M.then(P.string("OUTPUTLOG")).map(ast.outputLog).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETTIME")).map(ast.getTime).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETMILLISECOND")).map(ast.getMillisecond).skip(r.EOL),
+		r.Whitespace0M.then(P.string("GETSECOND")).map(ast.getSecond).skip(r.EOL),
+		r.Whitespace0M.then(P.string("FORCEWAIT")).map(ast.forceWait).skip(r.EOL),
+		r.Whitespace0M.then(P.string("WAITANYKEY")).map(ast.waitAnyKey).skip(r.EOL),
+		r.Whitespace0M.then(P.string("DUMPRAND")).map(ast.dumpRand).skip(r.EOL),
+		r.Whitespace0M.then(P.string("INITRAND")).map(ast.initRand).skip(r.EOL),
+		r.Whitespace0M.then(P.string("DEBUGCLEAR")).map(ast.debugClear).skip(r.EOL),
+		r.Whitespace0M.then(P.string("MOUSEX")).map(ast.mouseX).skip(r.EOL),
+		r.Whitespace0M.then(P.string("MOUSEY")).map(ast.mouseY).skip(r.EOL),
+		r.Whitespace0M.then(P.string("ISACTIVE")).map(ast.isActive).skip(r.EOL),
+		r.Whitespace0M.then(P.string("CBGCLEAR")).map(ast.cbgClear).skip(r.EOL),
+		r.Whitespace0M.then(P.string("CBGCLEARBUTTON")).map(ast.cbgClearButton).skip(r.EOL),
+		r.Whitespace0M.then(P.string("CBGREMOVEBMAP")).map(ast.cbgRemoveBmap).skip(r.EOL),
+		r.Whitespace0M.then(P.string("CLEARTEXTBOX")).map(ast.clearTextBox).skip(r.EOL),
+		r.Whitespace0M.then(P.string("STRDATA")).map(ast.strData).skip(r.EOL),
+		r.Whitespace0M.then(P.string("STOPCALLTRAIN")).map(ast.stopCallTrain).skip(r.EOL),
 	),
-	MultiLine: (r) => r.Line.sepBy(P.newline).skip(P.eof),
+	Command: (r) => r.PlainCommand,
+	Statement: (r) => P.alt(
+		r.Label,
+		r.Goto,
+		r.Command,
+	),
+	Language: (r) => r.Statement.many().skip(P.eof),
 });
 
-export default function parse(content: string): ast.Node[] {
-	const lines = language.MultiLine.tryParse(content);
-	return lines.filter((line) => line != null) as ast.Node[];
+export default function parse(content: string): ast.Statement[] {
+	return language.Language.tryParse(content + "\n");
 }

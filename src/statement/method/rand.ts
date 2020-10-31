4b0f6b0e8a9af8486065acbf9d5654bf47547e41
Undercrow <undercrow@protonmail.com>
2020-11-01 00:40:14 +0900

    Fix RAND method's argument order

--- src/statement/method/rand.ts
+++ src/statement/method/rand.ts
@@ -1,11 +1,19 @@
-import {assertNumber} from "../../assert";
+import {assert, assertNumber} from "../../assert";
 import type VM from "../../vm";
 
 export default function rand(_vm: VM, arg: Array<string | number>): number {
-	const min = arg[0] ?? 0;
-	assertNumber(min, "1st argument of RAND should be an integer");
-	const max = arg[1] ?? ((2 ** 32) - 1);
-	assertNumber(max, "2nd argument of RAND should be an integer");
+	if (arg.length === 0) {
+		assert(false, "RAND should have at least 1 argument");
+	} else if (arg.length === 1) {
+		const max = arg[0];
+		assertNumber(max, "1st argument of RAND should be an integer");
+		return Math.floor(Math.random() * max);
+	} else {
+		const min = arg[0];
+		assertNumber(min, "1st argument of RAND should be an integer");
+		const max = arg[1];
+		assertNumber(max, "2nd argument of RAND should be an integer");
 
-	return Math.floor(Math.random() * (max - min)) + min;
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
 }

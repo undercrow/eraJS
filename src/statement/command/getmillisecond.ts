727e51965b2a733dfe9a75e096558dc0a28565bc
Undercrow <undercrow@protonmail.com>
2020-12-24 11:15:48 +0900

    Implement time related commands

--- src/statement/command/getmillisecond.ts
+++ src/statement/command/getmillisecond.ts
@@ -1,14 +1,21 @@
+import dayjs from "dayjs";
+
 import * as U from "../../erb/util";
+import type VM from "../../vm";
 import Statement from "../index";
 
+// 719162 is number of days from 0001-01-01 to 1970-01-01
+const UNIX_EPOCH = 719162 * 24 * 60 * 60;
 export default class GetMillisecond extends Statement {
 	public constructor(arg: string) {
 		super();
 		U.arg0R0().tryParse(arg);
 	}
 
-	public *run() {
-		throw new Error("GETMILLISECOND is not implemented yet!");
+	public *run(vm: VM) {
+		const now = dayjs();
+
+		vm.getValue("RESULT").set(vm, now.valueOf() + UNIX_EPOCH * 1000, [0]);
 
 		return null;
 	}
--- src/statement/command/gettime.ts
+++ src/statement/command/gettime.ts
@@ -1,4 +1,7 @@
+import dayjs from "dayjs";
+
 import * as U from "../../erb/util";
+import type VM from "../../vm";
 import Statement from "../index";
 
 export default class GetTime extends Statement {
@@ -7,8 +10,11 @@ export default class GetTime extends Statement {
 		U.arg0R0().tryParse(arg);
 	}
 
-	public *run() {
-		throw new Error("GETTIME is not implemented yet!");
+	public *run(vm: VM) {
+		const now = dayjs();
+
+		vm.getValue("RESULT").set(vm, parseInt(now.format("YYYYMMDDHHmmssSSS")), [0]);
+		vm.getValue("RESULTS").set(vm, now.format("YYYY年MM月DD日 HH:mm:ss"), [0]);
 
 		return null;
 	}

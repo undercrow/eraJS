9e2f4522d5ffdd96111145daa9d94195e39050c9
Undercrow <undercrow@protonmail.com>
2021-09-26 05:09:12 +0900

    Use expression as rgb component of SETCOLOR command

--- src/statement/command/setcolor.ts
+++ src/statement/command/setcolor.ts
@@ -7,17 +7,14 @@ import * as U from "../../erb/util";
 import Lazy from "../../lazy";
 import type VM from "../../vm";
 import type Expr from "../expr";
-import Const from "../expr/const";
 import Statement from "../index";
 
-const PARSER = P.alt(
-	U.arg3R3(U.UInt, U.UInt, U.UInt).map(
-		([r, g, b]) => new Const(color.toHex({r, g, b})),
-	),
+const PARSER = P.alt<Expr | [Expr, Expr, Expr]>(
+	U.arg3R3(E.expr, E.expr, E.expr),
 	U.arg1R1(E.expr),
 );
 export default class SetColor extends Statement {
-	public value: Lazy<Expr>;
+	public value: Lazy<Expr | [Expr, Expr, Expr]>;
 
 	public constructor(raw: string) {
 		super();
@@ -25,8 +22,21 @@ export default class SetColor extends Statement {
 	}
 
 	public *run(vm: VM) {
-		const value = this.value.get().reduce(vm);
-		assertNumber(value, "Argument of SETCOLOR must be an integer");
+		const parsed = this.value.get();
+		let value: number;
+		if (Array.isArray(parsed)) {
+			const r = parsed[0].reduce(vm);
+			const g = parsed[1].reduce(vm);
+			const b = parsed[2].reduce(vm);
+			assertNumber(r, "1st argument of SETCOLOR must be an integer");
+			assertNumber(g, "2nd argument of SETCOLOR must be an integer");
+			assertNumber(b, "3rd argument of SETCOLOR must be an integer");
+			value = color.toHex({r, g, b});
+		} else {
+			const rgb = parsed.reduce(vm);
+			assertNumber(rgb, "Argument of SETCOLOR must be an integer");
+			value = rgb;
+		}
 
 		vm.color.front = color.hex(value);
 

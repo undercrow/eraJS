b7ca3b26d7ec4ed462700f95d7f5e5f3e6e99a66
Undercrow <undercrow@protonmail.com>
2021-10-04 07:44:55 +0900

    Refactor preprocessor

--- src/erb/preprocess.ts
+++ src/erb/preprocess.ts
@@ -1,65 +1,71 @@
 export default function preprocess(raw: string, macros: Set<string>): string[] {
-	let temp: string[] = [];
-
-	// Step 1: Break by newline
-	const result1 = raw.replace(/\r\n/g, "\n").replace(/\r/g, "\n").split("\n");
-
-	// Step 2: Strip comments
-	const result2 = result1.map((line) => line.replace(/;.*$/, ""));
-
-	// Step 3: Trim leading/trailing whitespaces
-	const result3 = result2.map((line) => line.replace(/^\s+/, "").replace(/\s+$/, ""));
-
-	// Step 4: Remove empty lines
-	const result4 = result3.filter((line) => line !== "");
-
-	// Step 5: Remove [SKIPSTART]~[SKIPEND] and [IF_DEBUG]~[ENDIF] lines
-	const result5: string[] = [];
-	temp = result4.slice();
-	while (temp.length > 0) {
-		const line = temp.shift()!;
-		if (line === "[SKIPSTART]") {
-			[, temp] = splitBy(temp, "[SKIPEND]");
-		} else if (line === "[IF_DEBUG]") {
-			[, temp] = splitBy(temp, "[ENDIF]");
-		} else {
-			result5.push(line);
-		}
-	}
-
-	// Step 6: Check [IF]/[ENDIF] blocks
-	// TODO: Handle [ELSEIF], [ELSE]
-	let result6: string[] = [];
-	temp = result5.slice();
-	while (temp.length > 0) {
-		const line = temp.shift()!;
-		if (/^\[IF .*\]$/.test(line)) {
-			const name = line.slice("[IF ".length, -1 * "]".length);
-			const [body, rest] = splitBy(temp, "[ENDIF]");
-			if (macros.has(name)) {
-				result6 = result6.concat(body);
+	const utf8 = raw.replace(/\uFEFF|\uFFEF/g, "");
+	const lines = utf8.replace(/\r|\n/g, "\n").split("\n");
+	const fn: Array<(prev: string[]) => string[]> = [
+		// Strip comments
+		(prev) => prev.map((line) => line.replace(/;.*$/, "")),
+		// Trim whitespaces
+		(prev) => prev.map((line) => line.trim()),
+		// Remove empty lines
+		(prev) => prev.filter((line) => line !== ""),
+		// Remove [SKIPSTART]~[SKIPEND] and [IF_DEBUG]~[ENDIF] lines
+		(prev) => {
+			const result: string[] = [];
+			let temp = prev.slice();
+			while (temp.length > 0) {
+				const line = temp.shift()!;
+				if (line === "[SKIPSTART]") {
+					[, temp] = splitBy(temp, "[SKIPEND]");
+				} else if (line === "[IF_DEBUG]") {
+					[, temp] = splitBy(temp, "[ENDIF]");
+				} else {
+					result.push(line);
+				}
 			}
-			temp = rest;
-		} else {
-			result6.push(line);
-		}
-	}
 
-	// Step 7: Concatenate lines inside braces
-	const result7: string[] = [];
-	temp = result6.slice();
-	while (temp.length > 0) {
-		const line = temp.shift()!;
-		if (line === "{") {
-			const [group, rest] = splitBy(temp, "}");
-			result7.push(group.join(""));
-			temp = rest;
-		} else {
-			result7.push(line);
-		}
-	}
+			return result;
+		},
+		// Check [IF]/[ENDIF] blocks
+		// TODO: Handle [ELSEIF], [ELSE]
+		(prev) => {
+			let result: string[] = [];
+			let temp = prev.slice();
+			while (temp.length > 0) {
+				const line = temp.shift()!;
+				if (/^\[IF .*\]$/.test(line)) {
+					const name = line.slice("[IF ".length, -1 * "]".length);
+					const [body, rest] = splitBy(temp, "[ENDIF]");
+					if (macros.has(name)) {
+						result = result.concat(body);
+					}
+					temp = rest;
+				} else {
+					result.push(line);
+				}
+			}
 
-	return result7;
+			return result;
+		},
+		// Concatenate lines inside braces
+		(prev) => {
+			const result: string[] = [];
+			let temp = prev.slice();
+			while (temp.length > 0) {
+				const line = temp.shift()!;
+				if (line === "{") {
+					const [group, rest] = splitBy(temp, "}");
+					result.push(group.join(""));
+					temp = rest;
+				} else {
+					result.push(line);
+				}
+			}
+
+			return result;
+		},
+	];
+
+	return fn.reduce((acc, val) => val(acc), lines);
 }
 
 function splitBy(lineList: string[], separator: string): [string[], string[]] {

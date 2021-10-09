bdc13c87b9aafc4cae93493c77f9776ff31b4c8f
Undercrow <undercrow@protonmail.com>
2021-10-09 23:55:56 +0900

    Save save data message to RESULTS in CHKDATA

--- src/statement/command/chkdata.ts
+++ src/statement/command/chkdata.ts
@@ -2,7 +2,7 @@ import * as assert from "../../assert";
 import * as E from "../../erb/expr";
 import * as U from "../../erb/util";
 import Lazy from "../../lazy";
-import {savefile, BaseSave} from "../../savedata";
+import {savefile, GameSave} from "../../savedata";
 import Int0DValue from "../../value/int-0d";
 import type VM from "../../vm";
 import type Expr from "../expr";
@@ -22,30 +22,38 @@ export default class ChkData extends Statement {
 		assert.number(index, "1st argument of CHKDATA must be a number");
 
 		let result: number;
+		let message: string = "";
 		const file = savefile.game(index);
 		const raw = vm.external.getSavedata(file);
 		if (raw == null) {
 			result = 0;
+			message = "----";
 		} else {
 			try {
-				const parsed: BaseSave = JSON.parse(raw);
+				const parsed: GameSave = JSON.parse(raw);
 				assert.number(parsed.code, `Save file ${file} is not in a valid format`);
 				assert.number(parsed.version, `Save file ${file} is not in a valid format`);
+				assert.string(parsed.data.comment, `Save file ${file} is not in a valid format`);
 				const code = vm.getValue<Int0DValue>("GAMEBASE_GAMECODE").get(vm, []);
 				const version = vm.getValue<Int0DValue>("GAMEBASE_VERSION").get(vm, []);
 				if (parsed.code !== code) {
 					result = 2;
+					message = "異なるゲームのセーブデータです";
 				} else if (parsed.version !== version) {
 					result = 3;
+					message = "セーブデータのバーションが異なります";
 				} else {
 					result = 0;
+					message = parsed.data.comment;
 				}
 			} catch {
 				result = 4;
+				message = "読み込み中にエラーが発生しました";
 			}
 		}
 
 		vm.getValue("RESULT").set(vm, result, [0]);
+		vm.getValue("RESULTS").set(vm, message, [0]);
 
 		return null;
 	}

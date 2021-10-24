f0a15f5739c1debceaec3fbe5bdd0f6f6abe373b
Undercrow <undercrow@protonmail.com>
2021-10-24 15:54:29 +0900

    Implement PICKUPCHARA command

--- src/statement/command/pickupchara.ts
+++ src/statement/command/pickupchara.ts
@@ -1,12 +1,14 @@
-import * as E from "../../error";
+import * as assert from "../../assert";
+import type Character from "../../character";
 import * as X from "../../parser/expr";
 import * as U from "../../parser/util";
 import Lazy from "../../lazy";
 import Slice from "../../slice";
+import type VM from "../../vm";
 import type Expr from "../expr";
 import Statement from "../index";
 
-const PARSER = U.argNR0(X.expr);
+const PARSER = U.argNR1(X.expr, X.expr);
 export default class PickupChara extends Statement {
 	public arg: Lazy<Expr[]>;
 
@@ -16,8 +18,35 @@ export default class PickupChara extends Statement {
 		this.arg = new Lazy(raw, PARSER);
 	}
 
-	public *run() {
-		throw E.notImpl("PICKUPCHARA");
+	public *run(vm: VM) {
+		const arg = this.arg.get().map((expr) => expr.reduce(vm));
+		let target = -1;
+		let assi = -1;
+		let master = -1;
+		const characterList: Character[] = [];
+		for (let i = 0; i < arg.length; ++i) {
+			const index = arg[i];
+			assert.number(index, `${i + 1}th argument of PICKUPCHARA should be a number`);
+			assert.cond(
+				index >= 0 && index < vm.characterList.length,
+				`${i + 1}th argument of PICKUPCHARA is out of range`,
+			);
+
+			if (index === vm.getValue("TARGET").get(vm, [])) {
+				target = i;
+			}
+			if (index === vm.getValue("ASSI").get(vm, [])) {
+				assi = i;
+			}
+			if (index === vm.getValue("MASTER").get(vm, [])) {
+				master = i;
+			}
+			characterList.push(vm.characterList[i]);
+		}
+		vm.getValue("TARGET").set(vm, target, []);
+		vm.getValue("ASSI").set(vm, assi, []);
+		vm.getValue("MASTER").set(vm, master, []);
+		vm.characterList = characterList;
 
 		return null;
 	}
